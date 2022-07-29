8ccfe5070e3d36bbbdba7ebfe0f9510a4b2cea88
NguyenTrung89 <trungnguyen.hust@gmail.com>
2022-07-29 18:10:14 +0700

    Update 05_flow_chart

--- 05_flow_chart.c
+++ 05_flow_chart.c
@@ -3,16 +3,14 @@
 
 int main(){
 	int num, result = 0;
-	int check = 0;
-	do{
-		printf("Input num:\n>>");
+	while(1){
+		printf("Input num: ");
 		scanf("%d", &num);
 		if(num >= 0 && num < 100){
 			result++;
-			check = 1;
 		}
-		else check = 0;
-	}while(check == 1);
+		else break;
+	}
 	
 	printf("Result = %d", result);
 	
